4326e1b6afbcaeff7572992a6dfb44998b9274b0
Omay Edekar <omayedekar@gmail.com>
2018-12-18 21:42:49 -0500

    "rescale chessboard and screen"

--- grid.py
+++ grid.py
@@ -1,7 +1,7 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((900,900))
+screen = pygame.display.set_mode((600,600))
 clock = pygame.time.Clock()
 chessboard = pygame.image.load("chessboard.png")
 done = False
