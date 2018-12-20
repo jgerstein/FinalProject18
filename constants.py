6b3a1dbdaaf6a6e33c4c0adbfddceb393897d476
Omay Edekar <omayedekar@gmail.com>
2018-12-19 19:27:45 -0500

    renamed functions.py to constants.py; redid chessboard and pieces; created basic sprite in player.py; added more constants to constants.py

--- constants.py
+++ constants.py
@@ -4,6 +4,11 @@ import sys
 
 pygame.init()
 
+pos_x_list = [45,90,135,180,225,270,315,360,405,450,495,540,585,630,675,720]
+pos_y_list = [45,90,135,180,225,270,315,360,405,450,495,540,585,630,675,720]
+axis_list = ['x', 'y']
+far_close_list = [45, 720]
+
 _image_library = {}
 def get_image(path):
         global _image_library
--- player.py
+++ player.py
@@ -1,10 +1,42 @@
 import pygame
-import functions
+import constants
+import random
 
 pygame.init()
-screen = pygame.display.set_mode((600,600))
+screen = pygame.display.set_mode((810, 810))
+pygame.display.set_caption("Copycat Chess")
 clock = pygame.time.Clock()
+all_sprites_list = pygame.sprite.Group()
 done = False
+
+pos_x_list = constants.pos_x_list
+pos_y_list = constants.pos_y_list
+
+print(pos_x_list)
+print(pos_y_list)
+
+class Piece(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = constants.get_image("pieces/pd.png")
+        self.rect = self.image.get_rect()
+
+for i in range(20):
+    print(i)
+    piece = Piece()
+    axis = random.choice(constants.axis_list)
+    if axis == 'x':
+        rect_x = random.choice(pos_x_list)
+        piece.rect.x = rect_x
+        piece.rect.y = random.choice(constants.far_close_list)
+        pos_x_list.remove(rect_x)
+    elif axis == 'y':
+        rect_y = random.choice(pos_y_list)
+        piece.rect.x = random.choice(constants.far_close_list)
+        piece.rect.y = rect_y
+        pos_y_list.remove(rect_y)
+
+    all_sprites_list.add(piece)
 
 while not done:
     for event in pygame.event.get():
@@ -14,8 +46,10 @@ while not done:
     if pressed[pygame.K_SPACE]:
         done = True
 
-    screen.blit(functions.get_image("chessboard.png"), (0,0))
-        
+    screen.blit(constants.get_image("chessboard.png"), (0,0))
+    all_sprites_list.update()
+    all_sprites_list.draw(screen)
+
     pygame.display.flip()
     clock.tick(60)
 
